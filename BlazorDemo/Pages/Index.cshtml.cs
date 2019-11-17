faadf4b7716b1e812f1078d3e0119e820d41cfbc
Brian Balderston <brian.j.balderston.civ@mail.mil>
2019-11-17 10:07:33 -0500

    Use RenderMode.Server and add Post for testing

--- BlazorDemo/Pages/Index.cshtml.cs
+++ BlazorDemo/Pages/Index.cshtml.cs
@@ -1,25 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace BlazorDemo.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private SessionState _state;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public string UserId { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, SessionState state)
         {
             _logger = logger;
+            _state = state;
         }
 
         public void OnGet()
         {
+        }
 
+        public async void OnPost()
+        {
         }
     }
 }
