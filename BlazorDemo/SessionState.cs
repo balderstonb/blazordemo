2bee3e0ea81866f8496c58c08a2a64936a5dcd2f
Brian Balderston <brian.j.balderston.civ@mail.mil>
2019-11-17 10:04:59 -0500

    Store dirty words in sessionstate

---
+++ BlazorDemo/SessionState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemo
+{
+    public class SessionState
+    {
+        private Dictionary<string, List<string>> dirtyWords = new Dictionary<string, List<string>>();
+
+        public List<string> GetDirtyWords(string key)
+        {
+            return dirtyWords[key];
+        }
+
+        public void SetDirtyWords(string key, List<string> words)
+        {
+            if (!dirtyWords.ContainsKey(key))
+            {
+                dirtyWords.Add(key, words);
+            }
+            else
+            {
+                dirtyWords[key] = words;
+            }
+        }
+    }
+}
